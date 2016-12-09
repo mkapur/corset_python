837fdcb9540324896499da58975133d56a71624b
M Kapur <mkapur@users.noreply.github.com>
2016-12-09 08:23:39 -1000

    Delete globme.py

--- globme.py
+++
@@ -1,9 +0,0 @@
-import os
-rootdir = 'C:\Users\mkapur\Dropbox\_CORSET\modeloutputs'
-gp = arcgisscripting.create() 
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-		if file.endswith(".txt") and file.startswith("test_spatial_"):
-			print os.path.join(subdir, file)
-			arcpy.ASCIIToRaster_conversion(os.path.join(files,file), os.path.join(files,file.rsplit(".")[0][13:]), "FLOAT")
-			
