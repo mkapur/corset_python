e7eac7649643520ef2d31c849075da743bbfea40
M Kapur <mkapur@users.noreply.github.com>
2016-12-29 15:17:10 -1000

    Update generateMPAnuts.py

--- python scripts/Generate MPA-Nut/generateMPAnuts.py
+++ python scripts/Generate MPA-Nut/generateMPAnuts.py
@@ -1,7 +1,7 @@
-##A script to randomly generate reef cell numbers for 5%, 10%, and 20% MPA coverage, given a number of total cells
-##This saves them in CORSET-ready format into the mk_code file or wherever specified.
-##M Kapur 08 Oct 2015
-
+# A script to randomly generate reef cell numbers for 5%, 10%, and 20% MPA coverage, given a number of total cells. 
+# This saves them in CORSET-ready format.
+## M Kapur 08 Oct 2015
+## Update 29 Dec 2016
 import random
 import os
 
@@ -9,11 +9,12 @@ percentages = [0.05, 0.10, 0.20] ##assign the percentage values
 
 numCells = 571 ##it is zero-indexed so this will automatically account for that. Change this as needed based on res.
 
-##MPAs
+## MPAs
 for p in percentages:
 	reefCells = range(1, numCells) ##generates a list from 0 to x
-	mpaCells = random.sample(reefCells, int(numCells * p)) ##takes a random sample from your cell range of the appropriate size, coerced in integere\save_path = "C:/Users/mkapur/Dropbox/_CORSET/mk_code/" ##where you want to save it
-	save_path = 'C:/Users/mkapur/Dropbox/_CORSET/mk_code/'
+	##takes a random sample from your cell range of the appropriate size, coerced to integer 
+	mpaCells = random.sample(reefCells, int(numCells * p)) 
+	save_path = 'C:/Users/mkapur/Dropbox/_CORSET/mk_code/' ##where you want to save it
 	completeName = os.path.join(save_path + str(int(p*100)) +"mpaCells"+".txt") ##the name of the file
 	text_file = open(completeName, 'w')
 	text_file.write("\n".join(str(x) for x in mpaCells)) ##write the elements in a single column text file
@@ -23,9 +24,9 @@ for p in percentages:
 ##nutrification
 for p in percentages:
 	reefCells = range(1, numCells) ##generates a list from 0 to x
-	nutCells = random.sample(reefCells, int(numCells * p)) ##takes a random sample from your cell range of the appropriate size, coerced in integere\save_path = "C:/Users/mkapur/Dropbox/_CORSET/mk_code/" ##where you want to save it
+	nutCells = random.sample(reefCells, int(numCells * p)) 
 	save_path = 'C:/Users/mkapur/Dropbox/_CORSET/mk_code/'
-	completeName = os.path.join(save_path + str(int(p*100)) +"nutCells"+".txt") ##the name of the file
+	completeName = os.path.join(save_path + str(int(p*100)) +"nutCells"+".txt")
 	text_file = open(completeName, 'w')
 	text_file.write("year\taffected cell IDs (list)\n") ## write in the title plus a line break
 	for y in range(1,101):
